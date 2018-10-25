ecdf9290accc11236f09373bedf3515d50592932
suhan <suhan@DESKTOP-SU34KMU>
2018-10-25 18:21:56 +0900

    gd

---
+++ src/test/Rectangle.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Rectangle {
+private int width;
+
+public int getWidth() {
+	return width;
+}
+
+public void setWidth(int width) {
+	this.width = width;
+}
+public int calc(){
+	return this.width * this.width;
+}
+
+}
---
+++ src/test/triangle2.java
@@ -0,0 +1,17 @@
+package test;
+
+public class triangle2 {
+
+	private int height;
+
+	public int getheight() {
+		return height;
+	}
+
+	public void setheight(int height) {
+		this.height = height;
+	}
+	public int calc(){
+		return this.height * width;
+	}
+}
